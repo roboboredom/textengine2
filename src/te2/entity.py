d568c927b38ac8c99cb0313135b14b2833d9f150
roboboredom <57413665+roboboredom@users.noreply.github.com>
2020-10-05 11:47:33 -0500

    rand fix

--- src/te2/entity.py
+++ src/te2/entity.py
@@ -3,7 +3,7 @@ class BaseEntity:
   Base class for an entity which holds data for a handler to read.
   """
   def __init__(self, name=None, symbol=None, x=0, y=0, shell=False): #called on instance creation
-    if not shell:
+    if shell != True:
       self.name = name
       self.symbol = symbol #symbol representing this object
       self.x = x
